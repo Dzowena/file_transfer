c6a083c7e5c6835b7fe276f87f4a757623f9121e
Dzowena <lukadzotsenidze1999@gmail.com>
2025-07-23 16:54:39 +0400

    Initial commit

---
+++ src/main/java/ge/luka/filetransferapp/FiletransferappApplication.java
@@ -0,0 +1,13 @@
+package ge.luka.filetransferapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransferappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletransferappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/luka/filetransferapp/FiletransferappApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.luka.filetransferapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiletransferappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
